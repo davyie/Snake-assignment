e63972c76578e78d0f38e61c0756a21eef828abd
LenaRay <photoinstockholm@gmail.com>
2020-10-27 22:14:56 +0100

    fix for filling in [0][0] cell

--- src/main/java/Snake/GamePanel.java
+++ src/main/java/Snake/GamePanel.java
@@ -252,8 +252,8 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (running) {
-            move();
             checkApple();
+            move();
             checkCollisions();
         }
         repaint();
